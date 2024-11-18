fe9c07fa15decca12971c73151a5083046683dfe
je-lam <jemian.lam@gmail.com>
2024-11-18 10:59:38 -0800

    Removed junk assets

--- scripts/classcontent.js
+++ scripts/classcontent.js
@@ -17,11 +17,13 @@ function closeAllAccordions() {
     for (i = 0; i < detailArray.length; i++) {
         detailArray[i].open = false;
     }
-    expandAllButton.style.borderImage = "url(../Math-13-Smolarski-Page/assets/icons/Plus_Border_Outward.png) 20 round";
+    expandAllButton.style.borderImage = "url(../Math-13-Smolarski-Page/assets/icons/Plus_Border_Expand.png) 20 round";
+    return;
 }
 function expandAllAccordions() {
     for (i = 0; i < detailArray.length; i++) {
         detailArray[i].open = true;
     }
-    expandAllButton.style.borderImage = "url(../Math-13-Smolarski-Page/assets/icons/Minus_Border_Inward.png) 20 round";
+    expandAllButton.style.borderImage = "url(../Math-13-Smolarski-Page/assets/icons/Minus_Border_Collapse.png) 20 round";
+    return;
 }
