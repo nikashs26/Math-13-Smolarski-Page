8278a865c00573045f29ac5a8e8ff49400ba8691
je-lam <jemian.lam@gmail.com>
2024-11-18 17:59:19 -0800

    Preparing checkbox styling

--- scripts/classcontent.js
+++ scripts/classcontent.js
@@ -1,7 +1,25 @@
 const detailArray = document.getElementsByTagName("details");
 const expandAllButton = document.getElementById("expandall-toggler");
+let numTimesExpandToggleClicked = 0; //Even is EXPAND ALL, Odd is COLLAPSE ALL 
+
+// HOVER EFFECTS
+expandAllButton.addEventListener('mouseover', (event) => {
+    if (numTimesExpandToggleClicked % 2 == 0) {
+        expandAllButton.style.borderImage = "url(../Math-13-Smolarski-Page/assets/borders/Plus_Border_Expand3.png) 20 round";
+    } else {
+        expandAllButton.style.borderImage = "url(../Math-13-Smolarski-Page/assets/borders/Minus_Border_Collapse3.png) 20 round";
+    }
+})
+
+expandAllButton.addEventListener('mouseout', (event) => {
+    if (numTimesExpandToggleClicked % 2 == 0) {
+        expandAllButton.style.borderImage = "url(../Math-13-Smolarski-Page/assets/borders/Plus_Border_Expand.png) 20 round";
+    } else {
+        expandAllButton.style.borderImage = "url(../Math-13-Smolarski-Page/assets/borders/Minus_Border_Collapse.png) 20 round";
+    }
+})
 let i = 0;
-let numTimesExpandToggleClicked = 0;
+
 function toggleExpandAllAccordions() {
     for (i = 0; i < detailArray.length; i++) {
         if (numTimesExpandToggleClicked % 2 == 0) {
@@ -17,13 +35,15 @@ function closeAllAccordions() {
     for (i = 0; i < detailArray.length; i++) {
         detailArray[i].open = false;
     }
-    expandAllButton.style.borderImage = "url(../Math-13-Smolarski-Page/assets/icons/Plus_Border_Expand.png) 20 round";
+    expandAllButton.style.borderImage = "url(../Math-13-Smolarski-Page/assets/borders/Plus_Border_Expand3.png) 20 round";
+    expandAllButton.innerHTML = 'EXPAND ALL';
     return;
 }
 function expandAllAccordions() {
     for (i = 0; i < detailArray.length; i++) {
         detailArray[i].open = true;
     }
-    expandAllButton.style.borderImage = "url(../Math-13-Smolarski-Page/assets/icons/Minus_Border_Collapse.png) 20 round";
+    expandAllButton.style.borderImage = "url(../Math-13-Smolarski-Page/assets/borders/Minus_Border_Collapse3.png) 20 round";
+    expandAllButton.innerHTML = 'COLLAPSE ALL';
     return;
 }
