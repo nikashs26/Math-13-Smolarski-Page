c4e5981f63b65499df17ad40a402b1690ced6de5
je-lam <jemian.lam@gmail.com>
2024-11-18 22:37:53 -0800

    Added reduced motion settings for accessibility purposes

--- scripts/classcontent.js
+++ scripts/classcontent.js
@@ -1,6 +1,10 @@
 const detailArray = document.getElementsByTagName("details");
+const summaryArray = document.getElementsByTagName("summary"); //This is for preventing seizures with the transitions.
 const expandAllButton = document.getElementById("expandall-toggler");
 let numTimesExpandToggleClicked = 0; //Even is EXPAND ALL, Odd is COLLAPSE ALL 
+
+const query = window.matchMedia(
+    '(prefers-reduced-motion: no-preference)');
 
 // HOVER EFFECTS
 expandAllButton.addEventListener('mouseover', (event) => {
@@ -18,8 +22,9 @@ expandAllButton.addEventListener('mouseout', (event) => {
         expandAllButton.style.borderImage = "url(../Math-13-Smolarski-Page/assets/borders/Minus_Border_Collapse.png) 20 round";
     }
 })
-let i = 0;
 
+// "EXPAND/COLLAPSE ALL" BUTTON
+let i = 0;
 function toggleExpandAllAccordions() {
     for (i = 0; i < detailArray.length; i++) {
         if (numTimesExpandToggleClicked % 2 == 0) {
@@ -33,10 +38,16 @@ function toggleExpandAllAccordions() {
 
 function closeAllAccordions() {
     for (i = 0; i < detailArray.length; i++) {
+        summaryArray[i].style.transition = "none"; //seizure prevention in action
         detailArray[i].open = false;
     }
     expandAllButton.style.borderImage = "url(../Math-13-Smolarski-Page/assets/borders/Plus_Border_Expand3.png) 20 round";
     expandAllButton.innerHTML = 'EXPAND ALL';
+    if (query.matches) {
+        for (i = 0; i < detailArray.length; i++) {
+            summaryArray[i].style.transition = "0.1s"; //resetting "out" transition after everything collapses
+        }
+    }
     return;
 }
 function expandAllAccordions() {
@@ -47,3 +58,6 @@ function expandAllAccordions() {
     expandAllButton.innerHTML = 'COLLAPSE ALL';
     return;
 }
+
+// SHOW/HIDE PREVIOUS WEEKS
+let currDate = Date.now();
