c5539d67a56bbf5aa2fd88d76441f0c02b48c018
PatPrograming <30586247+PatPrograming@users.noreply.github.com>
2024-11-20 23:59:51 -0800

    almost done with server

--- smolarski-page/src/index.ts
+++ smolarski-page/src/index.ts
@@ -1,17 +1,43 @@
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
 import { serveStatic } from '@hono/node-server/serve-static'
+import fs from 'fs';
+import path from 'path';
 
 const app = new Hono()
-
-
-app.use('/*', serveStatic({
-  root: './static'
-}))
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
+
+app.use('/*', serveStatic({ 
+  root: './static/HTML'
+}));
+
+app.use('/*', serveStatic({ 
+  root: './static/assets'
+}));
+
+app.use('/*', serveStatic({ 
+  root: './static/css'
+}));
+
+app.use('/*', serveStatic({ 
+  root: './static/icons'
+}));
+
+app.use('/*', serveStatic({ 
+  root: './static/screenshots'
+}));
+
+app.use('/*', serveStatic({ 
+  root: './static/screenshots copy'
+}));
+
+app.use('/*', serveStatic({ 
+  root: './static/scripts'
+}));
+
 
 const port = 3000
 console.log(`Server is running on http://localhost:${port}`)
